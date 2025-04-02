64b6090a25dd572140d19907fb54c0513f55c0e6
sai <saivaraprasad072@gmail.com>
2025-04-02 18:06:08 +0530

    small bugs fixed (add operation Error)

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 const fs = require('fs');
 const { v4: uuidv4 } = require("uuid");
 
@@ -8,11 +7,10 @@ const PORT = 3000;
 const FILE_PATH = './Todo.json';
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public')); 
-app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-// Function to read JSON file
 const readTodos = () => {
     try {
         const data = fs.readFileSync(FILE_PATH, 'utf8');
@@ -23,69 +21,70 @@ const readTodos = () => {
     }
 };
 
-// Function to write JSON file
 const writeTodos = (todos) => {
+    try{
     fs.writeFileSync(FILE_PATH, JSON.stringify(todos, null, 2));
+    }
+    catch(err){
+         console.log("Error writing File: ", err);
+    }
 };
 
 app.get('/', (req, res) => {
-    let todos = readTodos();
-    res.render('index.ejs', { todos });
+    res.render('index.ejs', { todos: readTodos() });
 });
 
-app.delete('/delete/:id', (req, res) => {
-    const id = req.params.id;
-    let todos = readTodos();
-    const updatedTodos = todos.filter(todo => todo.id !== id);
-    if (todos.length === updatedTodos.length) {
-        return res.status(404).json({ message: "Task not found" });
+app.post("/add", (req, res) => {
+    const { todo } = req.body;
+    console.log("Received task:", req.body);
+    if (!todo) {
+        return res.status(400).json({ success: false, message: "Task cannot be empty" });
     }
-    writeTodos(updatedTodos);
-    res.status(200).json({ message: "Task deleted successfully" });
+
+    let todos = readTodos();
+    const newTodo = { id: uuidv4(), task: todo };
+
+    todos.push(newTodo);
+    writeTodos(todos);
+
+    res.status(201).json({ success: true, todo: newTodo });
 });
 
 app.post("/edit/:id", (req, res) => {
-    let todos = readTodos();  
+    let todos = readTodos();
     let taskId = req.params.id;
     let updatedTask = req.body.task;
 
     let taskIndex = todos.findIndex(todo => todo.id === taskId);
     if (taskIndex !== -1) {
         todos[taskIndex].task = updatedTask;
-        writeTodos(todos); 
+        writeTodos(todos);
         return res.json({ success: true });
-    } else {
-        return res.json({ success: false });
     }
+    return res.json({ success: false });
 });
 
-app.post("/:id",(req,res)=>{
-  let todos = readTodos();
-  let taskId = req.params.id;
-  let taskIndex = todos.findIndex(todo => todo.id === taskId);
-  if(taskIndex !== -1) {
-      return res.json({ success: true , task :todos[taskIndex].task });
-  } else {
-      return res.json({ success: false});
-  }
-});
-app.post("/add", (req, res) => {
-    const { todo } = req.body;
-    if (!todo) {
-        return res.status(400).json({ success: false, message: "Task cannot be empty" });
-    }
-
+app.get("/fetch/:id", (req, res) => {
     let todos = readTodos();
-    const newTodo = {
-        id: uuidv4(),
-        task: todo
-    };
+    let taskId = req.params.id;
+    let task = todos.find(todo => todo.id === taskId);
 
-    todos.push(newTodo);
-    writeTodos(todos);
+    if (task) {
+        return res.json({ success: true, task: task.task });
+    } else {
+        return res.status(404).json({ success: false, message: "Task not found" });
+    }
+});
 
-    // Send a JSON response back to the client
-    res.status(201).json({ success: true, todo: newTodo });
+
+app.delete('/delete/:id', (req, res) => {
+    let todos = readTodos();
+    let updatedTodos = todos.filter(todo => todo.id !== req.params.id);
+    if (todos.length === updatedTodos.length) {
+        return res.status(404).json({ message: "Task not found" });
+    }
+    writeTodos(updatedTodos);
+    res.status(200).json({ message: "Task deleted successfully" });
 });
 
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
